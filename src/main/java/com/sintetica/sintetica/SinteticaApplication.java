7beba5eae1c58dfae636f8b62db6dba9b5339366
Axel Chavarria <chavarriamontoyaaxel01@gmail.com>
2024-03-13 11:38:50 -0600

    Base structure

---
+++ src/main/java/com/sintetica/sintetica/SinteticaApplication.java
@@ -0,0 +1,13 @@
+package com.sintetica.sintetica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinteticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SinteticaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sintetica/sintetica/SinteticaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sintetica.sintetica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SinteticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
